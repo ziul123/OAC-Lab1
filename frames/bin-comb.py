356a12a5746129bafbfc6e7189507f60a59ea219
Luiz <luizcsnvartuli@gmail.com>
2021-09-01 11:31:14 -0300

    outros errinhos

--- frames/bin-comb.py
+++ frames/bin-comb.py
@@ -1,10 +1,11 @@
 import os
+import platform as p
 FOLDER_PATH = dir_path = os.path.dirname(os.path.realpath(__file__))
 print(FOLDER_PATH)
 os.chdir(FOLDER_PATH)
 files = sorted([x for x in os.listdir(FOLDER_PATH) if x[-4:] == ".bin"], key=lambda x:int(x[2:-4]))
 
-if os.name == "posix":
+if p.system() == "Linux":
     delete = "rm -f "
 else:
     delete = "del /q "
--- frames/bmp2bin.py
+++ frames/bmp2bin.py
@@ -1,10 +1,11 @@
 import os
+import platform as p
 FOLDER_PATH = dir_path = os.path.dirname(os.path.realpath(__file__))
 print(FOLDER_PATH)
 os.chdir(FOLDER_PATH)
 imgs = [x[:-4] for x in os.listdir(FOLDER_PATH) if x[-4:] == ".bmp"]
 
-if os.name == "posix":
+if p.system() == "Linux":
     bmp = "wine bmp2oac2 "
     delete = "rm -f *bmp *.s *.mif"
 else:
